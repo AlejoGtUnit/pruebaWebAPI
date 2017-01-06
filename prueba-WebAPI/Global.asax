6254cf3f9ac91c22971064d53bfda91585166694
Alejandro Hurtado <alejogtunit460@gmail.com>
2017-01-06 13:41:38 -0600

    Commit Inicial

---
+++ prueba-WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="prueba_WebAPI.Global" Language="C#" %>
---
+++ prueba-WebAPI/ResourceXController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace prueba_WebAPI
+{
+    public class ResourceXController : ApiController
+    {
+        public string Get()
+        {
+            return string.Format("Respuesta desde ResourceXController->Get()->{0}", System.DateTime.Now.ToLongDateString());
+        }
+    }
+}
